fed655dbba6272cb19704a51c4e15efd7d70a1c3
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-22 21:11:07 +0000

    feat(main): add locale

--- src/main.c
+++ src/main.c
@@ -3,15 +3,19 @@
  *   All rights reserved.
  */
 
+#include <locale.h>
 #include <stdio.h>
 
 #include "fen.h"
 #include "game.h"
 
 int main(int argc, char *argv[]) {
+  setlocale(LC_ALL, "");
+
   for (int i = 0; i < argc; ++i) {
     printf("arg %i = %s\n", i, *argv++);
   }
+
   Game game;
   initGame(&game, FEN_STARTING_POSITION);
   return 0;
