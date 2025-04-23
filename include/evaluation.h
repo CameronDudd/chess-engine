51f4c1da22dfaca872dbd10cfcbd53129d27a84a
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-23 16:31:39 +0000

    feat(chess-engine): add placeholders for search and evaluation

---
+++ include/evaluation.h
@@ -0,0 +1,8 @@
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef EVALUATION_H
+#define EVALUATION_H
+#endif  // EVALUATION_H
---
+++ include/search.h
@@ -0,0 +1,8 @@
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef SEARCH_H
+#define SEARCH_H
+#endif  // SEARCH_H
