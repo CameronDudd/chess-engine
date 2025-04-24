3a2afacc023dc27c117305c51e1d640c4dcc6356
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-24 22:24:24 +0000

    feat(game): moev state into single uint8_t

--- include/game.h
+++ include/game.h
@@ -9,14 +9,22 @@
 #include "board.h"
 #include "fen.h"
 
+typedef enum {
+  ACTIVE_COLOR_BIT           = 0,  // 0 if black to move else 1
+  WHITE_KINGSIDE_CASTLE_BIT  = 1,  // 1 if white can castle king-side else 0
+  WHITE_QUEENSIDE_CASTLE_BIT = 2,  // 1 if white can castle queen-side else 0
+  BLACK_KINGSIDE_CASTLE_BIT  = 3,  // 1 if black can castle king-side else 0
+  BLACK_QUEENSIDE_CASTLE_BIT = 4,  // 1 if black can castle queen-side else 0
+} GameStateBitOffsets;
+
 typedef struct {
   Position src;
   Position dest;
 } Move;
 
 typedef struct {
-  int activeColor;
   Board board;
+  uint8_t state;
 } Game;
 
 void initGame(Game *game, Fen *fen);
--- src/game.c
+++ src/game.c
@@ -7,6 +7,14 @@
 #include "fen.h"
 
 void initGame(Game *game, Fen *fen) {
-  game->activeColor = fenActiveColor(fen);
+  game->state = 0;                // Reset state
+  if (fenActiveColor(fen) == 1) { // White to move
+    game->state |= (1 << ACTIVE_COLOR_BIT);
+  }
+  game->state |= fenCastlingBits(fen);
   fenPopulateBoard(fen, game->board);
+}
+
+uint8_t activeTurn(Game *game) {
+  return ((game->state & (1 << ACTIVE_COLOR_BIT)) == 0) ? 0 : 1;
 }
