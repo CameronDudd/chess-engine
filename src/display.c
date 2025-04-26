c9e3714da38611b5bfbb27e79eb550b76e3dd6c8
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-26 06:50:49 +0000

    feat(display): use better colors in other display funcs

--- src/display.c
+++ src/display.c
@@ -41,11 +41,16 @@ void displaySelectedPieces(const Board board, const Piece *selectedPiece) {
   for (Position row = 0; row < ROWS; ++row) {
     for (Position col = 0; col < COLS; ++col) {
       Piece p = board[(row * COLS) + col];
-      const char *bg = ((row + col) % 2) ? "\033[100m" : "\033[47m";
+      const char *pieceColor = (p & PIECE_WHITE)
+                                   ? _rgbEscapeStringForeground(255, 255, 255)
+                                   : _rgbEscapeStringForeground(0, 0, 0);
+      const char *boardColor = ((row + col) % 2)
+                                   ? _rgbEscapeStringBackground(181, 136, 99)
+                                   : _rgbEscapeStringBackground(240, 217, 181);
       if (p == *selectedPiece) {
-        printf("%s(%s)\033[0m", bg, pieceStr(&p));
+        printf("%s%s(%s)\033[0m", pieceColor, boardColor, pieceStr(&p));
       } else {
-        printf("%s %s \033[0m", bg, pieceStr(&p));
+        printf("%s%s %s \033[0m", pieceColor, boardColor, pieceStr(&p));
       }
     }
     printf("\n");
@@ -57,12 +62,17 @@ void displaySelectedPosition(const Board board,
   for (Position row = 0; row < ROWS; ++row) {
     for (Position col = 0; col < COLS; ++col) {
       Position position = (row * COLS) + col;
-      Piece piece = board[position];
-      const char *bg = ((row + col) % 2) ? "\033[100m" : "\033[47m";
+      Piece p = board[position];
+      const char *pieceColor = (p & PIECE_WHITE)
+                                   ? _rgbEscapeStringForeground(255, 255, 255)
+                                   : _rgbEscapeStringForeground(0, 0, 0);
+      const char *boardColor = ((row + col) % 2)
+                                   ? _rgbEscapeStringBackground(181, 136, 99)
+                                   : _rgbEscapeStringBackground(240, 217, 181);
       if (position == *selectedPosition) {
-        printf("%s(%s)\033[0m", bg, pieceStr(&piece));
+        printf("%s%s(%s)\033[0m", pieceColor, boardColor, pieceStr(&p));
       } else {
-        printf("%s %s \033[0m", bg, pieceStr(&piece));
+        printf("%s%s %s \033[0m", pieceColor, boardColor, pieceStr(&p));
       }
     }
     printf("\n");
