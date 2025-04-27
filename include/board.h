0ced90ebe0641cb3cba24b3165776e75c51c2bab
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-27 22:15:07 +0000

    feat(board): create Direction enum

--- include/board.h
+++ include/board.h
@@ -18,12 +18,23 @@ typedef int Position;  // 0 top-left to 63 bottom-right
 typedef uint64_t BitBoard;
 typedef Piece Board[NUM_POSITIONS];
 
-extern BitBoard pieceBitBoards[NUM_UNIQUE_PIECES];
+typedef enum {
+  FORWARD        = -8,
+  FORWARD_RIGHT  = -7,
+  RIGHT          = 1,
+  BACKWARD_RIGHT = 9,
+  BACKWARD       = 8,
+  BACKWARD_LEFT  = 7,
+  LEFT           = -1,
+  FORWARD_LEFT   = -9,
+} Direction;
 
 typedef struct {
   Position src;
   Position dst;
 } Move;
+
+extern BitBoard pieceBitBoards[NUM_UNIQUE_PIECES];
 
 void clearBoard(Board board);
 void initBitBoards(Board board);
