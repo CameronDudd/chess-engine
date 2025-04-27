3d47e907201d445cf8324531b909322e0f6d6f52
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-27 12:23:31 +0000

    feat(main): initBitBoards

--- src/main.c
+++ src/main.c
@@ -4,19 +4,27 @@
  */
 
 #include <locale.h>
+#include <log.h>
+#include <stdint.h>
 
+#include "board.h"
 #include "display.h"
 #include "fen.h"
 #include "game.h"
-#include "log.h"
 
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "");
   for (int i = 0; i < argc; ++i) {
     log_debug("arg %i = %s\n", i, *argv++);
   }
+
+  // Init
   Game game;
   initGame(&game, FEN_STARTING_POSITION);
+  initBitBoards(game.board);
+
+  // Main
   displayBoard(game.board);
+
   return 0;
 }
