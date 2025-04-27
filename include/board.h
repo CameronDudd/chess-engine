c07ea459f76cc42736cad35e8d00870fb68d8912
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-27 12:18:50 +0000

    feat(board): add piece position bitboards

--- include/board.h
+++ include/board.h
@@ -10,18 +10,22 @@
 
 #include "piece.h"
 
-#define ROWS       8
-#define COLS       8
-#define NUM_PIECES 64
+#define ROWS          8
+#define COLS          8
+#define NUM_POSITIONS 64
 
-typedef uint8_t Position;  // 0 top-left to 63 bottom-right
-typedef Piece Board[NUM_PIECES];
+typedef int Position;  // 0 top-left to 63 bottom-right
+typedef uint64_t BitBoard;
+typedef Piece Board[NUM_POSITIONS];
+
+extern BitBoard pieceBitBoards[NUM_UNIQUE_PIECES];
 
 typedef struct {
   Position src;
   Position dst;
 } Move;
 
+void initBitBoards(Board board);
 void makeMove(Board board, const Move *move);
 void strMakeMove(Board board, const char *moveStr);
 
--- src/board.c
+++ src/board.c
@@ -4,9 +4,59 @@
  */
 
 #include "board.h"
+#include "piece.h"
 
 #include <log.h>
 #include <stdlib.h>
+
+uint64_t pieceBitBoards[NUM_UNIQUE_PIECES] = {
+    0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+    0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+    0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+    0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+};
+
+static PieceLookupKey _piece2lookup(Piece piece) {
+  switch (piece) {
+  case PIECE_BLACK | PIECE_ROOK:
+    return BLACK_ROOK_KEY;
+  case PIECE_BLACK | PIECE_KNIGHT:
+    return BLACK_KNIGHT_KEY;
+  case PIECE_BLACK | PIECE_BISHOP:
+    return BLACK_BISHOP_KEY;
+  case PIECE_BLACK | PIECE_QUEEN:
+    return BLACK_QUEEN_KEY;
+  case PIECE_BLACK | PIECE_KING:
+    return BLACK_KING_KEY;
+  case PIECE_BLACK | PIECE_PAWN:
+    return BLACK_PAWN_KEY;
+  case PIECE_WHITE | PIECE_ROOK:
+    return WHITE_ROOK_KEY;
+  case PIECE_WHITE | PIECE_KNIGHT:
+    return WHITE_KNIGHT_KEY;
+  case PIECE_WHITE | PIECE_BISHOP:
+    return WHITE_BISHOP_KEY;
+  case PIECE_WHITE | PIECE_QUEEN:
+    return WHITE_QUEEN_KEY;
+  case PIECE_WHITE | PIECE_KING:
+    return WHITE_KING_KEY;
+  case PIECE_WHITE | PIECE_PAWN:
+    return WHITE_PAWN_KEY;
+  default:
+    log_error("Invalid piece '%i'", piece);
+    exit(1);
+  }
+}
+
+void initBitBoards(Board board) {
+  for (Position position = 0; position < NUM_POSITIONS; ++position) {
+    Piece piece = board[position];
+    if (piece != PIECE_NULL) {
+      PieceLookupKey lookupKey = _piece2lookup(piece);
+      pieceBitBoards[lookupKey] |= ((uint64_t)1 << position);
+    }
+  }
+}
 
 void makeMove(Board board, const Move *move) {
   board[move->dst] = board[move->src];
