2809b1c4d1c5a5c93c2c8efce23df647609532df
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-04-27 12:21:39 +0000

    feat(display): add displayPieceBitBoard

--- src/display.c
+++ src/display.c
@@ -40,6 +40,31 @@ void displayBoard(const Board board) {
   printf("   A  B  C  D  E  F  G  H\n");
 }
 
+void displayPieceBitBoard(const Board board, const BitBoard bitBoard) {
+  printf("\n");
+  for (Position row = 0; row < ROWS; ++row) {
+    printf("%i ", ROWS - row);
+    for (Position col = 0; col < COLS; ++col) {
+      Position boardPos = (row * COLS) + col;
+      Piece p = board[boardPos];
+      const char *pieceColor = (p & PIECE_WHITE)
+                                   ? _rgbEscapeStringForeground(255, 255, 255)
+                                   : _rgbEscapeStringForeground(0, 0, 0);
+      const char *boardColor;
+      if (bitBoard & (1ul << boardPos)) {
+        boardColor = _rgbEscapeStringBackground(255, 0, 0);
+      } else {
+        boardColor = ((row + col) % 2)
+                         ? _rgbEscapeStringBackground(181, 136, 99)
+                         : _rgbEscapeStringBackground(240, 217, 181);
+      }
+      printf("%s%s %s \033[0m", pieceColor, boardColor, pieceStr(&p));
+    }
+    printf("\n");
+  }
+  printf("   A  B  C  D  E  F  G  H\n");
+}
+
 void displaySelectedPieces(const Board board, const Piece *selectedPiece) {
   printf("\n");
   for (Position row = 0; row < ROWS; ++row) {
