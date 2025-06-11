c704292b74921f091ef7f81430e39ed73701f949
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-11 21:06:57 +0100

    fix(game): set moves to 0 in finite time

--- src/game.c
+++ src/game.c
@@ -5,17 +5,19 @@
 
 #include "game.h"
 
+#include "board.h"
+
 void initGame(Game *game, Fen *fen) {
-  game->state = 0;                // Reset state
-  if (fenActiveColor(fen) == 1) { // White to move
+  game->state = 0;                 // Reset state
+  if (fenActiveColor(fen) == 1) {  // White to move
     game->state |= (1 << ACTIVE_COLOR_BIT);
   }
   game->state |= fenCastlingBits(fen);
   fenPopulateBoard(fen, game->board);
-  game->legalMoves.count = 0;
+  game->legalMoves.count    = 0;
   game->previousMoves.count = 0;
-  for (size_t i = 0; i < 0; ++i) {
-    game->legalMoves.moves[i] = 0;
+  for (size_t i = 0; i < MAX_CHESS_MOVES; ++i) {
+    game->legalMoves.moves[i]    = 0;
     game->previousMoves.moves[i] = 0;
   }
 }
