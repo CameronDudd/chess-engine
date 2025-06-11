4216f74ef011540c5d200ccfcea10d6f91ef8219
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-11 23:12:33 +0100

    feat(chess-engine): change to move encoding

--- tests/perftTests.c
+++ tests/perftTests.c
@@ -3,23 +3,55 @@
  *   All rights reserved.
  */
 
+// perft first move results from stockfish
 // perft results from https://www.chessprogramming.org/Perft_Results
 
 #include <unity.h>
 #include <unity_fixture.h>
 
+#include "board.h"
 #include "fen.h"
 #include "perft.h"
 
+#define FEN_A2A3_POSITION "rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR b KQkq - 0 1"
+#define FEN_B2B3_POSITION "rnbqkbnr/pppppppp/8/8/8/1P6/P1PPPPPP/RNBQKBNR b KQkq - 0 1"
+#define FEN_C2C3_POSITION "rnbqkbnr/pppppppp/8/8/8/2P5/PP1PPPPP/RNBQKBNR b KQkq - 0 1"
+#define FEN_D2D3_POSITION "rnbqkbnr/pppppppp/8/8/8/3P4/PPP1PPPP/RNBQKBNR b KQkq - 0 1"
+#define FEN_E2E3_POSITION "rnbqkbnr/pppppppp/8/8/8/4P3/PPPP1PPP/RNBQKBNR b KQkq - 0 1"
+#define FEN_F2F3_POSITION "rnbqkbnr/pppppppp/8/8/8/5P2/PPPPP1PP/RNBQKBNR b KQkq - 0 1"
+#define FEN_H2H3_POSITION "rnbqkbnr/pppppppp/8/8/8/7P/PPPPPPP1/RNBQKBNR b KQkq - 0 1"
+#define FEN_A2A4_POSITION "rnbqkbnr/pppppppp/8/8/P7/8/1PPPPPPP/RNBQKBNR b KQkq a3 0 1"
+#define FEN_B2B4_POSITION "rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR b KQkq b3 0 1"
+#define FEN_C2C4_POSITION "rnbqkbnr/pppppppp/8/8/2P5/8/PP1PPPPP/RNBQKBNR b KQkq c3 0 1"
+#define FEN_D2D4_POSITION "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1"
+#define FEN_E2E4_POSITION "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+#define FEN_F2F4_POSITION "rnbqkbnr/pppppppp/8/8/5P2/8/PPPPP1PP/RNBQKBNR b KQkq f3 0 1"
+#define FEN_G2G4_POSITION "rnbqkbnr/pppppppp/8/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq g3 0 1"
+#define FEN_H2H4_POSITION "rnbqkbnr/pppppppp/8/8/7P/8/PPPPPPP1/RNBQKBNR b KQkq h3 0 1"
+#define FEN_B1A3_POSITION "rnbqkbnr/pppppppp/8/8/8/N7/PPPPPPPP/R1BQKBNR b KQkq - 0 1"
+#define FEN_B1C3_POSITION "rnbqkbnr/pppppppp/8/8/8/2N5/PPPPPPPP/R1BQKBNR b KQkq - 0 1"
+#define FEN_G1F3_POSITION "rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R b KQkq - 0 1"
+#define FEN_G1H3_POSITION "rnbqkbnr/pppppppp/8/8/8/7N/PPPPPPPP/RNBQKB1R b KQkq - 0 1"
+
 Board board;
+Move previousMove;
 
 TEST_GROUP(perftTests);
 
-TEST_SETUP(perftTests) { fenPopulateBoard(FEN_STARTING_POSITION, board); }
+TEST_SETUP(perftTests) {
+  previousMove = 0;
+  fenPopulateBoard(FEN_STARTING_POSITION, board);
+}
 
 TEST_TEAR_DOWN(perftTests) {}
 
 TEST(perftTests, perft0) {
+  // A2A3
+  fenPopulateBoard(FEN_A2A3_POSITION, board);
+  struct PerftRes perft0a2a3 = perft(board, fenActiveColor(FEN_A2A3_POSITION), 0, MOVE_ENCODE(coordinate2pos(""), coordinate2pos(""), 0, 0));
+  TEST_ASSERT_EQUAL_INT(1, perft0a2a3.nodes);
+
+  // Basic test
   struct PerftRes perft0 = perft(board, 1, 0, 0);
   TEST_ASSERT_EQUAL_INT(0, perft0.depth);
   TEST_ASSERT_EQUAL_UINT64(1, perft0.nodes);
@@ -50,6 +82,13 @@ TEST(perftTests, perft2) {
 }
 
 TEST(perftTests, perft3) {
+  // FIXME (cameron): this causes a caught error
+  // A2A3
+  fenPopulateBoard(FEN_A2A3_POSITION, board);
+  struct PerftRes perfta2a3 = perft(board, fenActiveColor(FEN_A2A3_POSITION), 3, MOVE_ENCODE(coordinate2pos("a2"), coordinate2pos("a3"), 0, 0));
+  TEST_ASSERT_EQUAL_INT(1, perfta2a3.nodes);
+
+  // Basic test
   struct PerftRes perft3 = perft(board, 1, 3, 0);
   TEST_ASSERT_EQUAL_INT(3, perft3.depth);
   TEST_ASSERT_EQUAL_UINT64(8902, perft3.nodes);
