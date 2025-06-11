272368d0dd3e42ed3abff4313e4c14f4907ebc85
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-11 23:12:49 +0100

    feat(main): set log level

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #include "perft.h"
 
 int main(int argc, char *argv[]) {
+  log_set_level(LOG_INFO);
+
   setlocale(LC_ALL, "");
   for (int i = 0; i < argc; ++i) {
     log_debug("arg %i = %s", i, *argv++);
@@ -19,15 +21,14 @@ int main(int argc, char *argv[]) {
 
   Board board;
   fenPopulateBoard(FEN_STARTING_POSITION, board);
-  log_debug("+-------+-------+----------+---------+--------+-------------+");
-  log_debug("| Depth | Nodes | Captures | Castles | Checks | Check Mates |");
-  log_debug("+-------+-------+----------+---------+--------+-------------+");
+  // TODO (cameron): use printf instead of log_info
+  log_info("+-------+-------+----------+---------+--------+-------------+");
+  log_info("| Depth | Nodes | Captures | Castles | Checks | Check Mates |");
+  log_info("+-------+-------+----------+---------+--------+-------------+");
   for (int depth = 0; depth < 4; ++depth) {
     struct PerftRes result = perft(board, 1, depth, 0);
-    log_debug("|%6i |%6llu |%9lu |%8lu |%7lu |%12lu |", result.depth,
-              result.nodes, result.captures, result.castles, result.checks,
-              result.checkMates);
+    log_info("|%6i |%6llu |%9lu |%8lu |%7lu |%12lu |", result.depth, result.nodes, result.captures, result.castles, result.checks, result.checkMates);
   }
-  log_debug("+-------+-------+----------+---------+--------+-------------+");
+  log_info("+-------+-------+----------+---------+--------+-------------+");
   return 0;
 }
