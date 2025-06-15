d3644215f323cadbfc2948de0bc8f5b7b9032edb
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-06-15 23:35:30 +0100

    feat(main): show white attack bit board after first move

--- src/main.c
+++ src/main.c
@@ -8,27 +8,22 @@
 #include <stdint.h>
 
 #include "board.h"
+#include "display.h"
 #include "fen.h"
-#include "perft.h"
+#include "game.h"
 
 int main(int argc, char *argv[]) {
-  log_set_level(LOG_INFO);
+  log_set_level(LOG_DEBUG);
 
   setlocale(LC_ALL, "");
   for (int i = 0; i < argc; ++i) {
     log_debug("arg %i = %s", i, *argv++);
   }
 
-  Board board;
-  fenPopulateBoard(FEN_STARTING_POSITION, board);
-  // TODO (cameron): use printf instead of log_info
-  log_info("+-------+-------+----------+---------+--------+-------------+");
-  log_info("| Depth | Nodes | Captures | Castles | Checks | Check Mates |");
-  log_info("+-------+-------+----------+---------+--------+-------------+");
-  for (int depth = 0; depth < 4; ++depth) {
-    struct PerftRes result = perft(board, 1, depth, 0);
-    log_info("|%6i |%6llu |%9lu |%8lu |%7lu |%12lu |", result.depth, result.nodes, result.captures, result.castles, result.checks, result.checkMates);
-  }
-  log_info("+-------+-------+----------+---------+--------+-------------+");
+  Game game;
+  initGame(&game, FEN_STARTING_POSITION);
+  generateLegalMoves(&game.legalMoves, game.board, activeColor(&game), game.previousMoves.moves[game.previousMoves.count - 1]);
+  displayBitBoard(game.board, colorAttackBitBoard(activeColor(&game)));
+
   return 0;
 }
