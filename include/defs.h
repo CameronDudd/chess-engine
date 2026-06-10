455b6687d65c346162dc47eb034c7d532cd06fe4
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2026-06-10 01:05:44 +0100

    fix(defs): include defs.h

---
+++ include/defs.h
@@ -0,0 +1,13 @@
+/*
+ *   Copyright (c) 2026 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdint.h>
+
+#define BIT_SQUARE(square) (((uint64_t)1) << (square))
+
+#endif  // DEFS_H
