a06be790156aa2e79d7943d6b929c71ccdd0ce5e
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2026-06-11 21:26:19 +0100

    feat(defs): move global definitions to defs

--- include/defs.h
+++ include/defs.h
@@ -8,6 +8,19 @@
 
 #include <stdint.h>
 
+#define NUM_POSITIONS 64  // 8 rows x 8 cols
+
+#define POS_INDEX(rank, file) (((rank) * 8) + (file))
+#define INDEX_RANK(index) ((index) / 8)
+#define INDEX_FILE(index) ((index) % 8)
 #define BIT_SQUARE(square) (((uint64_t)1) << (square))
+
+typedef uint8_t PositionIndex;  // 0 bottom-left to 63 top-right
+typedef uint64_t BitBoard;
+
+typedef enum {
+  WHITE = 0,
+  BLACK = 1,
+} Color;
 
 #endif  // DEFS_H
